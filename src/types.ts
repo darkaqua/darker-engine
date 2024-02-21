7e86f942621dcdc39d0a5c425289739310d8dad8
pagoru <pagoru@gmail.com>
2024-02-21 22:15:35 +0100

    wip #19

--- src/types.ts
+++ src/types.ts
@@ -2,24 +2,29 @@
  * Engine
  */
 export interface EngineType<I, C extends string | number, D> {
-	setSystems: (...systems: SystemFunction<C>[]) => void;
-
+	setSystems: (...systems: SystemFunction<C>[]) => Promise<void>;
+	
 	getEntityList: () => EntityType<I, C, D>[];
 	getEntityListByType: (type: I) => EntityType<I, C, D>[];
 	getEntityListByComponents: (...componentList: C[]) => EntityType<I, C, D>[];
-
-	getEntity: (id: number) => EntityType<I, C, D>;
-	addEntity: (...entities: SimpleEntityType<I, C, D>[]) => EntityType<I, C, D>[];
-	removeEntity: (...idList: number[]) => void;
-
+	
+	getEntity: (id: number) => EntityType<I, C, D> | undefined;
+	addEntity: (
+		...entities: SimpleEntityType<I, C, D>[]
+	) => Promise<EntityType<I, C, D>[]>;
+	removeEntity: (...idList: number[]) => Promise<void>;
+	
 	getSystem: (name: number) => SystemType<C> | undefined;
-
+	
 	clear: () => void;
-
-	load: () => void;
-	destroy: () => void;
-
-	getUID: () => number;
+	
+	load: () => Promise<void>;
+	hardReload: () => Promise<void>;
+	
+	__debug__: {
+		swapSystem: (systemId: number, system: SystemFunction<C>) => Promise<void>;
+		getSystem: (name: string) => SystemType<C>;
+	};
 }
 
 /**
@@ -27,16 +32,19 @@ export interface EngineType<I, C extends string | number, D> {
  */
 export interface SystemType<C> {
 	id: number;
+	name: string;
 	components: C[];
-	onAdd?: (id: number) => void;
-	onUpdate?: (id: number, component?: C) => void;
-	onRemove?: (id: number) => void;
-
-	onLoad?: () => void;
-	onDestroy?: () => void;
+	onAdd?: (id: number) => Promise<void>;
+	onUpdate?: (id: number, component?: C) => Promise<void>;
+	onRemove?: (id: number) => Promise<void>;
+	
+	onLoad?: () => Promise<void>;
+	onDestroy?: () => Promise<void>;
 }
 
-export type SystemFunction<C> = () => Omit<SystemType<C>, 'id'>;
+export type SystemFunction<C> = () => Promise<
+	Omit<SystemType<C>, 'id' | 'name'>
+>;
 
 /**
  * Entity
@@ -47,22 +55,22 @@ export interface EntityType<I, C extends string | number, D> {
 	readonly type: I;
 	readonly data: Partial<D>;
 	readonly components: C[];
-
-	id: number;
+	
+	id?: number;
+	safe?: boolean;
 	getData: () => Record<number, any>;
-	getComponent: <T extends keyof D>(
-		component: T,
-		deepClone?: boolean,
-	) => D[T];
+	getComponent: <T extends keyof D>(component: T, deepClone?: boolean) => D[T];
 	getComponents: <T extends keyof D>(
 		components: T[],
 		deepClone?: boolean,
 	) => { [K in T]: D[K] };
 	getComponentTypes: () => C[];
-	hasComponent: (component: C) => boolean;
-	hasComponents: (components: C[]) => boolean;
-	updateComponent: <T extends keyof D>(component: T, data?: Partial<D[T]>) => EntityType<I, C, D>;
-	removeComponent: (component: C) => void;
+	hasComponent: (component: number) => boolean;
+	updateComponent: <T extends keyof D>(
+		component: T,
+		data?: D[T],
+	) => Promise<EntityType<I, C, D> | undefined>;
+	removeComponent: (component: C) => Promise<void>;
 }
 
 export type SimpleEntityType<I, C extends string | number, D> = Omit<
@@ -72,11 +80,12 @@ export type SimpleEntityType<I, C extends string | number, D> = Omit<
 	| 'getComponents'
 	| 'getComponentTypes'
 	| 'hasComponent'
-	| 'hasComponents'
 	| 'updateComponent'
 	| 'removeComponent'
 >;
 
-export type EntityTypeFunction<I, C extends string | number, D> = () => SimpleEntityType<I, C, D>;
+export type EntityTypeFunction<I, C extends string | number, D, P> = (
+	props: P,
+) => SimpleEntityType<I, C, D>;
 
 export type DarkerMap<T extends string | number, S> = { [key in T]: S };
--- src/uid.ts
+++ src/uid.ts
@@ -1,14 +1,35 @@
-export const uid = (getEntityList) => {
-	let lastId = 0;
-
-	const getUID = () => {
-		do {
-			lastId++;
-		} while (getEntityList()[lastId]);
-		return lastId;
+export const uid = getEntityList => {
+	const lastSafeIdMap: Record<UIDKey, number> = {
+		[UIDKey.ENTITY]: 0,
+		[UIDKey.SYSTEM]: 0, //<< meh
 	};
-
+	const lastIdMap: Record<UIDKey, number> = {
+		[UIDKey.ENTITY]: 0,
+		[UIDKey.SYSTEM]: 0,
+	};
+	const idCheckMapFunc: Record<UIDKey, (currentId?: number) => boolean> = {
+		[UIDKey.ENTITY]: (currentId: number) => getEntityList()[currentId],
+		[UIDKey.SYSTEM]: () => false,
+	};
+	
+	const getUID = (key: UIDKey, safe: boolean = false) => {
+		const increment = safe ? 10_000 : 0;
+		
+		const _getUID = () =>
+			(safe ? lastSafeIdMap[key] : lastIdMap[key]) + increment;
+		do {
+			if (safe) lastSafeIdMap[key]++;
+			else lastIdMap[key]++;
+		} while (idCheckMapFunc[key](_getUID()));
+		return _getUID();
+	};
+	
 	return {
 		getUID,
 	};
 };
+
+export enum UIDKey {
+	ENTITY,
+	SYSTEM,
+}
